9369981f9783bc6058f6c9d958e1289cf0b027a0
dyerkebulan21 <dyerkebulan21@gmail.com>
2021-06-21 22:39:09 +0600

    mongo connect

--- core/db.ts
+++ core/db.ts
@@ -1,1 +1,16 @@
 import moongoose from 'mongoose'
+
+moongoose.Promise = Promise
+
+moongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/twitter', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true, 
+    useFindAndModify: false 
+})
+
+const db = moongoose.connection 
+
+db.on('error', console.error.bind(console, 'connection error'))
+
+export {db, moongoose}
