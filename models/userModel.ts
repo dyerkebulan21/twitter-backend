54d13605c8ebb7e5f2914b120546c733d2af6b23
dyerkebulan21 <dyerkebulan21@gmail.com>
2021-07-07 21:53:13 +0600

    features/hide password and confirmhash

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,52 +1,59 @@
-import {model, Schema, Document} from 'mongoose'
-
+import { model, Schema, Document } from "mongoose";
 
 export interface UserModelInterface {
-    email: string,
-    fullname: string,
-    username: string,
-    password: string,
-    confirmed_hash: string,
-    confirmed?: boolean,
-    location?: string,
-    about?: string,
-    website?:string
+  email: string;
+  fullname: string;
+  username: string;
+  password: string;
+  confirmed_hash: string;
+  confirmed?: boolean;
+  location?: string;
+  about?: string;
+  website?: string;
 }
 
-type UserModelDocumentInterface = UserModelInterface & Document
+type UserModelDocumentInterface = UserModelInterface & Document;
 
 const userSchema = new Schema({
-    email: {
-        unique: true,
-        required: true,
-        type: String
-    },
-    fullname: {
-        required: true,
-        type: String
-    },
-    username: {
-        unique: true,
-        required: true,
-        type: String
-    },
-    password: {
-        required: true,
-        type: String,
-        select: false
-    },
-    confirmed_hash: {
-        required: true,
-        type: String,
-        select: false
-    },
-    location: String,
-    confirmed: {
-        type: Boolean,
-        default: false
-    },
-    about: String,
-    website: String
-})
+  email: {
+    unique: true,
+    required: true,
+    type: String,
+  },
+  fullname: {
+    required: true,
+    type: String,
+  },
+  username: {
+    unique: true,
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+    select: false,
+  },
+  confirmed_hash: {
+    required: true,
+    type: String,
+    select: false,
+  },
+  location: String,
+  confirmed: {
+    type: Boolean,
+    default: false,
+  },
+  about: String,
+  website: String,
+});
 
-export const UserModel = model<UserModelDocumentInterface>('User', userSchema)
+userSchema.set("toJSON", {
+  transform: function (_, obj) {
+    delete obj.password;
+    delete obj.confirmed_hash;
+    return obj;
+  },
+});
+
+export const UserModel = model<UserModelDocumentInterface>("User", userSchema);
